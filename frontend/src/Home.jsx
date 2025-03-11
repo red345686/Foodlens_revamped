40b9355773b636e5c8258706453c5e9693d4d96e
Shreyas Jaiswal <shreyasjaiswal123@gmail.com>
2025-03-11 23:26:31 +0530

    Added background but navbar not good

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 const Home = () => {
+  // const myStyle = {
+  //   background: ("HomeBackground.png"),
+  //   // background: "center",
+  //   height: "100vh",
+  //   width: "100%",
+  //   background: "cover",
+  //   backgroundRepeat: "no-repeat",
+  // };
   return (
-    <div>
-      This is home page.
+    <div className="bg-[url('HomeBackground.png')] bg-cover bg-center fixed h-screen w-full">
     </div>
   )
 }
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,20 +2,18 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 function NavBar() {
   return (
-    <div className='container'>
-      <nav className='flex items-center justify-evenly text-black relative shadow-sm' role='navigation'>
-        <Link to='/' className='p-1'>
-          <div className='flex items-center'>
-            <img className="w-10 rounded-lg" src="logo.png" alt="" />
-            <span>FoodLens</span>
-          </div>
-        </Link>
-        <Link to='/products'>Products</Link>
-        <Link to='/blogs'>Blog</Link>
-        <Link to='/community'>Community</Link>
-        <Link to='/about'>About</Link>
-      </nav>
-    </div>
+    <nav className='flex items-center bg-transparent justify-evenly text-black sticky' role='navigation'>
+      <Link to='/' className='p-1'>
+        <div className='flex items-center'>
+          <img className="w-10 rounded-lg" src="logo.png" alt="" />
+          <span>FoodLens</span>
+        </div>
+      </Link>
+      <Link to='/products'>Products</Link>
+      <Link to='/blogs'>Blog</Link>
+      <Link to='/community'>Community</Link>
+      <Link to='/about'>About</Link>
+    </nav>
   )
 }
 
