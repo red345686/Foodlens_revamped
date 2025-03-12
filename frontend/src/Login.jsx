89b81fcf1fa052ec1a2c8e524b9b5aef23a7b888
Shreyas Jaiswal <shreyasjaiswal123@gmail.com>
2025-03-12 14:09:17 +0530

    Fixed some css in login page

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom'
 const Login = () => {
   return (
     <div className='flex justify-between'>
-      <div className='m-24'>
+      <div className='ml-24' style={{marginTop: '14vh'}}>
         <h1 className='text-4xl font-semibold'>Welcome back !</h1>
         <p className='font-serif mt-3'>Enter to get unlimited access to data and information</p>
-        <div className='flex flex-col space-y-1 mt-4'>
-          <label htmlFor="email" className='font-medium pl-3'>Email</label>
-          <input type="text" id='email' name='email' placeholder='Enter your email addresss' className='border rounded-lg h-10 px-4' />
+        <div className='flex flex-col space-y-1 mt-5'>
+          <label htmlFor="email" className='font-medium pl-3'>Email<span className='text-red-600'>*</span></label>
+          <input type="text" id='email' name='email' placeholder='Enter your email addresss' className='border rounded-lg h-10 p-6' />
         </div>
-        <div className='flex flex-col space-y-1 mt-4'>
-          <label htmlFor="password" className='font-medium pl-3'>Password</label>
-          <input type="text" id='password' name='password' placeholder='Enter your password' className='border rounded-lg h-10 px-4' />
+        <div className='flex flex-col space-y-1 mt-5'>
+          <label htmlFor="password" className='font-medium pl-3'>Password<span className='text-red-600'>*</span></label>
+          <input type="password" id='password' name='password' placeholder='Enter your password' className='border rounded-lg h-10 p-6' />
         </div>
         <div className='mx-2 flex justify-between font-thin text-sm mt-1'>
           <div>
@@ -30,7 +30,7 @@ const Login = () => {
         </div>
         {/* Google Login below */}
         <button className='flex justify-around border rounded-2xl text w-96' style={{ backgroundColor: '#f2f2f2' }}><img src='google.svg' style={{ width: '12rem' }}></img></button> 
-        <p>Don't have an account? <Link className='text-purple-600 underline font-semibold'>Register here</Link></p>
+        <p className='mt-6 mx-6'>Don't have an account? <Link className='text-purple-600 underline font-semibold'>Register here</Link></p>
       </div>
       <img src="LoginBack.jpg" alt="" style={{ height: '100vh' }} />
     </div>
