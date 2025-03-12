96ba77b0963868dd3654bbce193839481b392ebb
Shreyas Jaiswal <shreyasjaiswal123@gmail.com>
2025-03-12 14:55:53 +0530

    Still in login page

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div className='flex justify-between'>
-      <div className='ml-24' style={{marginTop: '14vh'}}>
+    <div className='lg:flex justify-between'>
+      <div className='mx-6 sm:mx-6 lg:mx-auto' style={{marginTop: '14vh'}}>
         <h1 className='text-4xl font-semibold'>Welcome back !</h1>
         <p className='font-serif mt-3'>Enter to get unlimited access to data and information</p>
         <div className='flex flex-col space-y-1 mt-5'>
-          <label htmlFor="email" className='font-medium pl-3'>Email<span className='text-red-600'>*</span></label>
-          <input type="text" id='email' name='email' placeholder='Enter your email addresss' className='border rounded-lg h-10 p-6' />
+          <label htmlFor="email" className='font-medium px-3'>Email<span className='text-red-600'>*</span></label>
+          <input type="text" id='email' name='email' placeholder='Enter your email address' className='border rounded-lg h-10 p-6' />
         </div>
         <div className='flex flex-col space-y-1 mt-5'>
-          <label htmlFor="password" className='font-medium pl-3'>Password<span className='text-red-600'>*</span></label>
+          <label htmlFor="password" className='font-medium px-3'>Password<span className='text-red-600'>*</span></label>
           <input type="password" id='password' name='password' placeholder='Enter your password' className='border rounded-lg h-10 p-6' />
         </div>
         <div className='mx-2 flex justify-between font-thin text-sm mt-1'>
@@ -22,17 +22,18 @@ const Login = () => {
           </div>
           <Link className='text-red-600'>Forgot your password?</Link>
         </div>
-        <button className='bg-lime-500 text-white px-auto py-2 rounded-lg mt-4 w-96'>Log In</button>
+        <button className='bg-lime-500 text-white px-auto py-2 rounded-lg mt-4 w-full'>Log In</button>
         <div className='flex justify-center flex-row my-4'>
-          <div style={{ width: '8rem', height: '0.1rem', marginTop: '0.7rem', backgroundColor: 'black' }}></div>
-          <p className='text-center mx-2'>Or Login With</p>
-          <div style={{ width: '8rem', height: '0.1rem', marginTop: '0.7rem', backgroundColor: 'black' }}></div>
+          <div className='w-full' style={{ height: '0.1rem', marginTop: '0.7rem', backgroundColor: 'black' }}></div>
+          <p className='text-center w-full'>Or Login With</p>
+          <div className='w-full' style={{ height: '0.1rem', marginTop: '0.7rem', backgroundColor: 'black' }}></div>
         </div>
         {/* Google Login below */}
-        <button className='flex justify-around border rounded-2xl text w-96' style={{ backgroundColor: '#f2f2f2' }}><img src='google.svg' style={{ width: '12rem' }}></img></button> 
+        <button className='flex justify-around border rounded-2xl text w-full' style={{ backgroundColor: '#f2f2f2' }}><img src='google.svg'></img></button> 
         <p className='mt-6 mx-6'>Don't have an account? <Link className='text-purple-600 underline font-semibold'>Register here</Link></p>
       </div>
-      <img src="LoginBack.jpg" alt="" style={{ height: '100vh' }} />
+      {/* <div className='bg-[url(LoginBack.jpg)]'></div> */}
+      <img className='lg:-z-10 lg:h-1' src="LoginBack.jpg" alt="" style={{ height: '100vh' }} />
     </div>
   )
 }
