82b94e003121f34c2f1affe37c198a5015051aab
Shreyas Jaiswal <shreyasjaiswal123@gmail.com>
2025-04-04 23:11:01 +0530

    abracadabra

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       <NavBar page='home' />
       <div className="bg-[url('HomeBackground.png')] bg-cover bg-center fixed h-screen w-[100vw]">
       </div>
-      <div className='fixed w-auto bg-transparent lg:p-10 ml-[10vw] mr-[10vw] sm:mr-[30vw] md:mr-[50vw] mt-[10vh] lg:mt-[20vh]'>
+      <div className='fixed w-auto bg-transparent lg:p-10 ml-[10vw] mr-[10vw] sm:mr-[30vw] md:mr-[50vw] h-screen flex flex-col justify-center text-center'>
         <div className='sm:w-auto text-center mt-[10vh] text-4xl sm:text-5xl md:text-6xl lg:text-8xl mb-10 text-white font-bold'>FoodLens</div>
         <div className='sm:w-auto pr-[10vw] sm:pr-0 text-xl md:text-2xl lg:text-3xl text-white font-normal font-sans'>FoodLens is your go-to platform for understanding the ingredients in your food. Empower yourself to eat smarter with FoodLens!</div>
         <div className='flex flex-col sm:flex-row sm:justify-evenly mt-10'>
